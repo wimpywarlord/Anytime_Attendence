d7029d420a862667c44fb437fe7063c6f01ddf36
wimpywarlord <wimpywarlord@gmail.com>
2019-08-13 02:20:40 +0530

    able to get att response

--- backend.js
+++ backend.js
@@ -1,4 +1,8 @@
+var bodyParser = require('body-parser');
 var mongoose =require("mongoose");
+var express= require("express");
+var app = express();
+
 mongoose.connect("mongodb://localhost/kshitij");
 
 var viit_att_schema = new mongoose.Schema({
@@ -6,6 +10,8 @@ var viit_att_schema = new mongoose.Schema({
 	no_of_present: Number,
 	no_of_absent: Number
 });
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 var viit_att= mongoose.model("viit_att",viit_att_schema);
 
@@ -23,17 +29,36 @@ viit_att.find({},function(err,viit_atts)
 	}
 });
 
-var express= require("express");
-var app = express();
+
 
 app.get("/",function(req,res)
 	{
 		res.render("viit_attendence.ejs");
 	});
+
 app.get("/attendence",function(req,res)
 	{	
 		res.render("take_att_page.ejs",{list_of_all_members:list_of_all_members});
 	});
+
+var use_posted_att;
+
+app.post("/post_att",function(req,res)
+{
+	res.redirect("/attendence");
+	// console.log(req.body);
+	use_posted_att = Object.keys(req.body.student);
+	console.log(use_posted_att);
+	for(var i=0;i < use_posted_att.length ;i++)
+	{
+		console.log(use_posted_att[i]);
+	}
+});
+
+
+
+
+
 
 app.listen(3000,function(){
 	console.log("SERVER 3000 HAS STARTED");
